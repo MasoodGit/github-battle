1be8a7364a2edfc803e55f846581ef86ade46508
Masood_Alam <mohamed.masood.alam@philips.com>
2017-12-27 18:41:55 +0530

    Initial commit

---
+++ app/components/Popular.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedLanguage: 'All'};
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState({selectedLanguage: lang});
+  }
+
+  render () {
+    var languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
+    return (
+      <ul className='languages'>
+        {
+          languages.map(function(lang) {
+            return (
+            <li
+              style={lang === this.state.selectedLanguage ? { color: '#d0021b'}: null}
+              onClick={this.updateLanguage.bind(null, lang)}
+              key={lang}>
+              { lang }
+            </li>
+            )
+          }, this)
+        }
+      </ul>
+    )
+  }
+}
+
+export default Popular;
